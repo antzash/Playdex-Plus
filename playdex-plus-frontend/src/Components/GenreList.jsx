303ad68c79bc3c916fe2ac74f364ac756ed6e2ac
antzash <antony.hashwin@gmail.com>
2024-04-03 19:57:30 +0800

    redo, fetch games by genre

--- playdex-plus-frontend/src/Components/GenreList.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-function GenreList() {
-  return <div>GenreList Component</div>;
-}
-
-export default GenreList;
--- playdex-plus-frontend/src/Pages/Home.jsx
+++ playdex-plus-frontend/src/Pages/Home.jsx
@@ -1,17 +1,11 @@
 import React from "react";
-import GameList from "../Components/GameList";
-import GenreList from "../Components/GenreList";
 
 const Home = () => {
   return (
     <div>
-      <div className="bg-red-400">
-        <GenreList />
-      </div>
+      <div className="bg-red-400"></div>
       <br />
-      <div className="bg-violet-300">
-        <GameList />
-      </div>
+      <div className="bg-violet-300"></div>
     </div>
   );
 };
