93d2df28c4968cfc427505e6602df0eacc662772
antzash <antony.hashwin@gmail.com>
2024-04-03 13:00:18 +0800

    fix home page

--- playdex-plus-frontend/src/Pages/Home.jsx
+++ playdex-plus-frontend/src/Pages/Home.jsx
@@ -1,7 +1,13 @@
 import React from "react";
 
 const Home = () => {
-  return <div className="text-black font-bold underline">Home Page</div>;
+  return (
+    <div>
+      <div className="bg-red-400">Genre List</div>
+      <br />
+      <div className="bg-violet-300">Game List</div>
+    </div>
+  );
 };
 
 export default Home;
