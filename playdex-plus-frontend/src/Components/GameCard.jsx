9b119c2ee4ba33100edb847c0a4c9dbcf07ad540
antzash <antony.hashwin@gmail.com>
2024-04-04 00:33:55 +0800

    basic gamecard design update

--- playdex-plus-frontend/src/Components/GameCard.jsx
+++ playdex-plus-frontend/src/Components/GameCard.jsx
@@ -10,18 +10,22 @@ const GameCard = ({ game }) => {
   return (
     <div
       key={game.id}
-      className="relative bg-green-600 rounded-lg group hover:scale-110 transition-all duration-300 ease-in-out cursor-pointer flex flex-col justify-between h-full"
+      className="relative rounded-lg group hover:scale-110 transition-all duration-300 ease-in-out cursor-pointer flex flex-col justify-between h-full"
     >
       <div>
         <img
           src={game.background_image}
           alt={game.name}
-          className="lazy h-[200px] w-full rounded-t-lg object-cover bg-green-900"
+          className="lazy h-[200px] w-full rounded-xl object-cover"
           loading="lazy"
         />
-        <h2 className="text-[20px] text-black font-bold p-2">{game.name}</h2>
+        <h2 className="text-[16px] text-left text-violet-800 font-bold p-2">
+          {game.name}
+        </h2>
       </div>
-      <div className="text-[10px] text-white font-light p-2">{platformStr}</div>
+      <div className="text-[10px] text-left text-black font-light p-2">
+        {platformStr}
+      </div>
     </div>
   );
 };
