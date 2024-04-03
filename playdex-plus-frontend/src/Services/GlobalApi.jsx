54519c86c4bdffd2be7b5609be9b5f1962943d1d
antzash <antony.hashwin@gmail.com>
2024-04-03 22:13:11 +0800

    redo

--- playdex-plus-frontend/src/Services/GlobalApi.jsx
+++ playdex-plus-frontend/src/Services/GlobalApi.jsx
@@ -6,13 +6,4 @@ const axiosCreate = axios.create({
   baseURL: "https://api.rawg.io/api/",
 });
 
-const getGenreList = axiosCreate.get("/genres?key=" + key);
-
-const getAllGames = () => {
-  return axiosCreate.get("games?key=" + key);
-};
-
-const getGamesbyGenreID = (id) =>
-  axiosCreate.get("games?key=" + key + "&genres=" + id);
-
-export default { getGenreList, getAllGames, getGamesbyGenreID };
+export default { getAllGames };
