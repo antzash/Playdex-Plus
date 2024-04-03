4dcf05f5d1f525286636c7bc605952b13e4a810b
antzash <antony.hashwin@gmail.com>
2024-04-03 20:03:18 +0800

    genre lsit can fetch genres

--- playdex-plus-frontend/src/Components/GenreList.jsx
+++ playdex-plus-frontend/src/Components/GenreList.jsx
@@ -1,6 +1,17 @@
-import React from "react";
+import React, { useEffect } from "react";
+import GlobalApi from "../Services/GlobalApi";
 
 function GenreList() {
+  useEffect(() => {
+    getGenreList();
+  }, []);
+
+  const getGenreList = () => {
+    // responsible for fetching genre list from api through GlobalApi.
+    GlobalApi.getGenreList.then((resp) => {
+      console.log(resp.data.results);
+    });
+  };
   return <div>GenreList Component</div>;
 }
 
