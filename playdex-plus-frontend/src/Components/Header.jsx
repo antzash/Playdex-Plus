15d34f412877ba2258ae0db5161ea38987bdf954
antzash <antony.hashwin@gmail.com>
2024-04-04 18:35:50 +0800

    search and conosle log results

--- playdex-plus-frontend/src/Components/Header.jsx
+++ playdex-plus-frontend/src/Components/Header.jsx
@@ -1,34 +1,51 @@
-import React from "react";
+import React, { useState } from "react";
 import { IoHeart } from "react-icons/io5";
-import { SiYoutubegaming } from "react-icons/si"; // Import the YouTube icon
+import { SiYoutubegaming } from "react-icons/si";
 import { Link } from "react-router-dom";
+import gameData from "../assets/gameData.json";
 
 function Header() {
+  const [searchTerm, setSearchTerm] = useState("");
+
+  function handleSearchInput(event) {
+    setSearchTerm(event.target.value);
+  }
+
+  function handleSearch(event) {
+    if (event.key === "Enter") {
+      const filteredGames = gameData.filter(
+        (game) =>
+          game.name.toLowerCase().includes(searchTerm.toLowerCase()) ||
+          game.tags.some((tag) =>
+            tag.name.toLowerCase().includes(searchTerm.toLowerCase())
+          )
+      );
+      console.log(filteredGames);
+      // Here you would update the game list to reflect the search results
+    }
+  }
+
   return (
     <div className="flex items-center justify-between p-4">
       <div className="flex items-center">
         <Link to="/">
-          {" "}
-          {/* Add this line */}
-          <SiYoutubegaming className="text-violet-800 font-bold text-[40px] mr-2" />{" "}
-          {/* Use the YouTube icon */}
-        </Link>{" "}
-        {/* Close the Link tag */}
+          <SiYoutubegaming className="text-violet-800 font-bold text-[40px] mr-2" />
+        </Link>
       </div>
       <div className="flex p-2 w-full mx-5 rounded-full">
         <input
           type="text"
           placeholder="Search..."
           className="w-full px-5 py-3 text-sm leading-tight text-gray-700 border rounded-xl shadow appearance-none focus:outline-none focus:shadow-outline"
+          value={searchTerm}
+          onChange={handleSearchInput}
+          onKeyPress={handleSearch}
         />
       </div>
       <div>
         <Link to="/playlist">
-          {" "}
-          {/* Add this line */}
           <IoHeart className="text-violet-800 text-[40px] p-1 rounded-full transition-transform duration-300 ease-in-out transform hover:scale-110 cursor-pointer" />
-        </Link>{" "}
-        {/* Close the Link tag */}
+        </Link>
       </div>
     </div>
   );
