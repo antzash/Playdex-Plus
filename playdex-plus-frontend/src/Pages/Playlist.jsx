f2d743ba7caf14696e9c5acefac4da56dc475c21
antzash <antony.hashwin@gmail.com>
2024-04-04 18:56:45 +0800

    add header to playlist page

--- playdex-plus-frontend/src/Pages/Playlist.jsx
+++ playdex-plus-frontend/src/Pages/Playlist.jsx
@@ -1,7 +1,12 @@
 import React from "react";
+import Header from "../Components/Header";
 
 function Playlist() {
-  return <div>Playlist</div>;
+  return (
+    <div>
+      <Header />
+    </div>
+  );
 }
 
 export default Playlist;
