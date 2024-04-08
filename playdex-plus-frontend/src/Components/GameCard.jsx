e87c9611d43c921ae175a2eb97ba4b7e370c1fc8
antzash <antony.hashwin@gmail.com>
2024-04-09 00:31:56 +0800

    view screenshots in gameCard.jsx

--- playdex-plus-frontend/src/Components/GameCard.jsx
+++ playdex-plus-frontend/src/Components/GameCard.jsx
@@ -1,5 +1,5 @@
 // GameCard.jsx
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { FaPlus } from "react-icons/fa"; // Import the plus icon
 
 const GameCard = ({ game, playlists, onAddToPlaylist }) => {
@@ -17,14 +17,43 @@ const GameCard = ({ game, playlists, onAddToPlaylist }) => {
   // Map over the genres array to create a string of genre names
   const genreStr = game.genres.map((genre) => genre.name).join(", ");
 
+  // State for managing hover and screenshot index
+  const [isHovered, setIsHovered] = useState(false);
+  const [currentScreenshotIndex, setCurrentScreenshotIndex] = useState(0);
+
+  // Effect to change screenshot on hover
+  useEffect(() => {
+    let intervalId;
+    if (
+      isHovered &&
+      game.short_screenshots &&
+      game.short_screenshots.length > 0
+    ) {
+      intervalId = setInterval(() => {
+        setCurrentScreenshotIndex(
+          (prevIndex) => (prevIndex + 1) % game.short_screenshots.length
+        );
+      }, 800); // Change screenshot every 2 seconds
+    }
+    return () => clearInterval(intervalId); // Cleanup on component unmount or hover end
+  }, [isHovered, game.short_screenshots]);
+
   return (
     <div
       key={game.id}
       className="p-5 relative rounded-lg group hover:scale-110 transition-all duration-300 ease-in-out cursor-pointer flex flex-col justify-between h-full shadow-xl hover:shadow-lg hover:text-white"
+      onMouseEnter={() => setIsHovered(true)}
+      onMouseLeave={() => setIsHovered(false)}
     >
       <div>
         <img
-          src={game.background_image}
+          src={
+            isHovered &&
+            game.short_screenshots &&
+            game.short_screenshots.length > 0
+              ? game.short_screenshots[currentScreenshotIndex].image
+              : game.background_image
+          }
           alt={game.name}
           className="lazy h-[200px] w-full rounded-xl object-cover"
           loading="lazy"
@@ -47,4 +76,5 @@ const GameCard = ({ game, playlists, onAddToPlaylist }) => {
     </div>
   );
 };
+
 export default GameCard;
