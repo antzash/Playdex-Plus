b4d3f1a318743a966c958ac3388dd05fda084b4b
antzash <antony.hashwin@gmail.com>
2024-04-09 21:45:17 +0800

    css styling for login and registration

--- playdex-plus-frontend/src/Components/Login.jsx
+++ playdex-plus-frontend/src/Components/Login.jsx
@@ -4,6 +4,7 @@ import UserContext from "../context/user";
 import { useState } from "react";
 import useFetch from "../hooks/useFetch";
 import { jwtDecode } from "jwt-decode";
+import logo from "../assets/Images/logo.png"; // Import the logo
 
 const Login = (props) => {
   const fetchData = useFetch();
@@ -26,47 +27,61 @@ const Login = (props) => {
 
   return (
     <>
-      <br />
-      <div className="row">
-        <div className="col-md-4"></div>
-        <input
-          type="text"
-          className="col-md-4"
-          placeholder="username"
-          value={username}
-          onChange={(e) => setUsername(e.target.value)}
-        />
-        <div className="col-md-4"></div>
-      </div>
-      <div className="row">
-        <div className="col-md-4"></div>
-        <input
-          type="password"
-          className="col-md-4"
-          placeholder="password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-        />
-        <div className="col-md-4"></div>
-      </div>
-      <div className="row">
-        <div className="col-md-4"></div>
-        <button className="col-md-4" type="submit" onClick={handleLogin}>
-          login
-        </button>
-        <div className="col-md-4"></div>
-      </div>
-      <br />
-      <div className="row">
-        <div className="col-md-4"></div>
-        <button
-          className="col-md-4"
-          type="submit"
-          onClick={() => props.setShowLogin(false)}
-        >
-          go to registration screen
-        </button>
-        <div className="col-md-4"></div>
+      <div className="flex flex-col items-center justify-center min-h-screen bg-whitesmoke">
+        <img src={logo} alt="Logo" className="mb-10 w-[500px] h-[150px]" />{" "}
+        {/* Display the logo */}
+        <div className="w-full max-w-md">
+          <div className="bg-white rounded px-8 pt-6 pb-8 mb-4">
+            <div className="mb-4">
+              <label
+                className="block text-violet-800 text-xl font-bold mb-2"
+                htmlFor="username"
+              >
+                Username
+              </label>
+              <input
+                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                id="username"
+                type="text"
+                placeholder="Username"
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
+              />
+            </div>
+            <div className="mb-6">
+              <label
+                className="block text-violet-800 text-xl font-bold mb-2"
+                htmlFor="password"
+              >
+                Password
+              </label>
+              <input
+                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
+                id="password"
+                type="password"
+                placeholder="Password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+            </div>
+            <div className="flex items-center justify-between">
+              <button
+                className="bg-whitesmoke hover:bg-violet-800 text-violet-800 hover:text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+                type="button"
+                onClick={handleLogin}
+              >
+                Sign In
+              </button>
+              <a
+                className="inline-block align-baseline font-bold text-sm text-black hover:text-blue-800"
+                href="#"
+                onClick={() => props.setShowLogin(false)}
+              >
+                Go to registration screen
+              </a>
+            </div>
+          </div>
+        </div>
       </div>
     </>
   );
--- playdex-plus-frontend/src/Components/Registration.jsx
+++ playdex-plus-frontend/src/Components/Registration.jsx
@@ -1,5 +1,6 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import useFetch from "../hooks/useFetch";
+import logo from "../assets/Images/logo.png"; // Import the logo
 
 const Registration = (props) => {
   const fetchData = useFetch();
@@ -7,81 +8,80 @@ const Registration = (props) => {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
 
-
-
   const registerUser = async () => {
     const res = await fetchData("/auth/register", "PUT", {
       username,
-      password
+      password,
     });
 
     if (res.ok) {
       setUsername("");
       setPassword("");
       alert("User successfully registered! Try logging in now.");
-      props.setShowLogin(true)
+      props.setShowLogin(true);
     } else {
       console.log(res.data);
-      alert("Registration failed. Errors encountered: \n" +res.data);
-
+      alert("Registration failed. Errors encountered: \n" + res.data);
     }
   };
 
-
   return (
     <>
-      <br />
-      <div className="row">
-        <div className="col-md-4"></div>
-        <input
-          className="col-md-4"
-          placeholder="username"
-          type="text"
-          value={username}
-          onChange={(e) => {
-            setUsername(e.target.value);
-          }}
-        />
-        <div className="col-md-4"></div>
-      </div>
-
-      <div className="row">
-        <div className="col-md-4"></div>
-        <input
-          className="col-md-4"
-          placeholder="password"
-          type="text"
-          value={password}
-          onChange={(e) => {
-            setPassword(e.target.value);
-          }}
-        />
-        <div className="col-md-4"></div>
-      </div>
-
-      <div className="row">
-       
-        <div className="col-md-4"></div>
-      </div>
-
-      <div className="row">
-        <div className="col-md-4"></div>
-        <button className="col-md-4" type="submit" onClick={registerUser}>
-          register
-        </button>
-        <div className="col-md-4"></div>
-      </div>
-
-      <div className="row">
-        <div className="col-md-4"></div>
-        <button
-          className="col-md-4"
-          type="submit"
-          onClick={() => props.setShowLogin(true)}
-        >
-          go to login screen
-        </button>
-        <div className="col-md-4"></div>
+      <div className="flex flex-col items-center justify-center min-h-screen">
+        <img src={logo} alt="Logo" className="mb-10 w-[500px] h-[150px]" />{" "}
+        {/* Display the logo */}
+        <div className="w-full max-w-md">
+          <div className="bg-whitesmoke rounded px-8 pt-6 pb-8 mb-4">
+            <div className="mb-4">
+              <label
+                className="block text-violet-800 text-xl font-bold mb-2"
+                htmlFor="username"
+              >
+                Username
+              </label>
+              <input
+                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                id="username"
+                type="text"
+                placeholder="Username"
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
+              />
+            </div>
+            <div className="mb-6">
+              <label
+                className="block text-violet-800 text-xl font-bold mb-2"
+                htmlFor="password"
+              >
+                Password
+              </label>
+              <input
+                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
+                id="password"
+                type="password"
+                placeholder="Password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+            </div>
+            <div className="flex items-center justify-between">
+              <button
+                className="bg-white hover:bg-violet-800 text-violet-800 hover:text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+                type="button"
+                onClick={registerUser}
+              >
+                Register
+              </button>
+              <a
+                className="inline-block align-baseline font-bold text-sm text-black hover:text-blue-800"
+                href="#"
+                onClick={() => props.setShowLogin(true)}
+              >
+                Go to login screen
+              </a>
+            </div>
+          </div>
+        </div>
       </div>
     </>
   );
