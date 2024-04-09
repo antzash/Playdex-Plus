0687ee40d1fded49dd8d4bae712def0e28b56078
eremus <eremus97@hotmail.com>
2024-04-09 11:44:18 +0800

    Update App.jsx

--- playdex-plus-frontend/src/App.jsx
+++ playdex-plus-frontend/src/App.jsx
@@ -4,16 +4,42 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import "./App.css";
 import Home from "./Pages/Home";
 import Playlist from "./Pages/Playlist";
+import UserContext from "./context/user";
+import { useState } from "react";
+import Login from "./Components/Login";
+import Registration from "./Components/Registration";
+
 
 function App() {
+  const [accessToken, setAccessToken] = useState("");
+  const [role, setRole] = useState("");
+  const [showLogin, setShowLogin] = useState(true); // toggle to regisration page
+
+
+
+
   return (
-    <Router>
+    <>
+    <UserContext.Provider
+        value={{ accessToken, setAccessToken, role, setRole }}
+      >
+    {accessToken.length > 0 && <Router>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/playlist" element={<Playlist />} />
       </Routes>
-    </Router>
-  );
+    </Router>}
+    {accessToken.length === 0 && showLogin && (
+          <Login setShowLogin={setShowLogin}></Login>
+        )}
+        {accessToken.length === 0 && !showLogin && (
+          <Registration setShowLogin={setShowLogin}></Registration>
+        )}
+
+
+    </UserContext.Provider>
+    </>
+    );
 }
 
 export default App;
